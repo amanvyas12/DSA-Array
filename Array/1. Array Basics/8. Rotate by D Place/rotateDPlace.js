572309bda384f61cf23a8c9596a3789f354d9a06
amanvyas12 <avyas1510march@gmail.com>
2025-05-22 23:34:13 +0530

    rotate array by d place

--- Array/1. Array Basics/8. Rotate by D Place/rotateDPlace.js
+++ Array/1. Array Basics/8. Rotate by D Place/rotateDPlace.js
@@ -1,14 +1,19 @@
 /* TO RUN THE JS FILE MAKE CHANEGS IN SCRIPTS{} UNDER START IN PACKAGE.JSON FILE */
 // Optimal solution which is having a better Time and space complexity then brute force.
 
-let arr = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let d = 3;
 
-function rotateDPlace(arr) {
-  let temp = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    arr[i - 1] = arr[i];
+function rotateDPlace(arr, d) {
+  d = d % arr.length; // handle d > arr.length
+  let temp = arr.slice(0, d); // store first d elements
+  for (let i = d; i < arr.length; i++) {
+    arr[i - d] = arr[i]; // shift the rest to the left
   }
-  arr[arr.length - 1] = temp;
+  for (let i = 0; i < temp.length; i++) {
+    arr[arr.length - d + i] = temp[i]; // put the stored elements at the end
+  }
   console.log(arr);
 }
-rotateDPlace(arr);
+rotateDPlace(arr, d);
+
