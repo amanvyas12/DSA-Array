817c0d44e824a11c3f952a138efc331973b1f752
amanvyas12 <avyas1510march@gmail.com>
2025-05-23 13:34:07 +0530

    move zero to end

---
+++ Array/1. Array Basics/9. Move Zeros to the end of Array/moveZerosToEnd.js
@@ -0,0 +1,23 @@
+/* TO RUN THE JS FILE MAKE CHANEGS IN SCRIPTS{} UNDER START IN PACKAGE.JSON FILE */
+// Optimal solution which is having a better Time and space complexity then brute force.
+
+let arr = [1, 2, 3, 4, 5, 0, 7, 5, 3, 0, 0, 0, 3, 3, 5, 5, 3]
+function moveZerosToEnd(arr) {
+    let j = -1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+    for (let i = j + 1; i < arr.length; i++) {
+        if (arr[i] != 0) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            j++;
+        }
+    }
+    return arr;
+    
+}
+moveZerosToEnd(arr);
+console.log(arr);
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // Arrays
 
 // Array Basics File running command list
-// /* 1 */ require('./Array/1. Array Basics/1. Check if Array is Sorted/sortedArray');
-// /* 2 */ require('./Array/1. Array Basics/2. Largest Element In an Array/largestElementInArray');
-// /* 3 */ require('./Array/1. Array Basics/3. Second Largest & smallest Element in an Array without sorting/secondLargestAndSmallestElement');
-// /* 4 */ require('./Array/1. Array Basics/4. Linear Search/linearSearch');
-// /* 5 */ require('./Array/1. Array Basics/5. Left Rotate an array by one place/leftRotateAnArray');
-// /* 6 */ require('./Array/1. Array Basics/6. Remove Duplicates from Sorted Array/removeDuplicatesFromSortedArray');
-// /* 7 */ require('./Array/1. Array Basics/7. Union of Two Sorted Arrays/unionOfTwoSortedArrays');
+/* 1 */ require('./Array/1. Array Basics/1. Check if Array is Sorted/sortedArray');
+/* 2 */ require('./Array/1. Array Basics/2. Largest Element In an Array/largestElementInArray');
+/* 3 */ require('./Array/1. Array Basics/3. Second Largest & smallest Element in an Array without sorting/secondLargestAndSmallestElement');
+/* 4 */ require('./Array/1. Array Basics/4. Linear Search/linearSearch');
+/* 5 */ require('./Array/1. Array Basics/5. Left Rotate an array by one place/leftRotateAnArray');
+/* 6 */ require('./Array/1. Array Basics/6. Remove Duplicates from Sorted Array/removeDuplicatesFromSortedArray');
+/* 7 */ require('./Array/1. Array Basics/7. Union of Two Sorted Arrays/unionOfTwoSortedArrays');
 /* 8 */ require('./Array/1. Array Basics/8. Rotate by D Place/rotateDPlace');
+/* 9 */ require('./Array/1. Array Basics/9. Move Zeros to the end of Array/moveZerosToEnd');
+
+
+
+
+
+
+
+
+
+
 
 // Array Easy File running command list
 /* 1 */
